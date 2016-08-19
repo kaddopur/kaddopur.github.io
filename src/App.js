1a870bac09375ce6e2817974ee731c9a6076dafb
Jason Huang <cjhuang@yahoo-inc.com>
2016-08-19 17:19:00 +0800

    change Logo to functional component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Logo from './components/Logo/Logo.js';
 
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,10 +1,8 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import './Logo.css';
 
-class Logo extends PureComponent {
-  render() {
-    return <div className="Logo">CHAOJU<br/>HUANG</div>
-  }
-}
+const Logo = () => (
+  <div className="Logo">CHAOJU<br/>HUANG</div>
+);
 
 export default Logo;
