096751fcce6efe8eed8ca747d53459ca1f295165
Jason Huang <cjhuang@yahoo-inc.com>
2016-08-19 18:03:44 +0800

    add SocialButtons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Logo from './components/Logo/Logo.js';
+import SocialButtons from './components/SocialButtons/SocialButtons.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Logo />
+        <SocialButtons />
       </div>
     );
   }
