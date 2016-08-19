c461de3beab2f23b39325c6279775d21152e4273
Jason Huang <cjhuang@yahoo-inc.com>
2016-08-19 17:14:16 +0800

    add editorconfig

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,10 +1,10 @@
 import React, { PureComponent } from 'react';
-import './logo.css';
+import './Logo.css';
 
 class Logo extends PureComponent {
-    render() {
-        return <div className="logo">CHAOJU<br/>HUANG</div>
-    }
+  render() {
+    return <div className="Logo">CHAOJU<br/>HUANG</div>
+  }
 }
 
 export default Logo;
