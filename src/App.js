0004fb068aef2d5da25e5a2644f3e88e53572483
Jason Huang <cjhuang@yahoo-inc.com>
2016-08-19 17:07:14 +0800

    add Logo

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Logo from './components/Logo/Logo.js';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Logo />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,10 @@
+import React, { PureComponent } from 'react';
+import './logo.css';
+
+class Logo extends PureComponent {
+    render() {
+        return <div className="logo">CHAOJU<br/>HUANG</div>
+    }
+}
+
+export default Logo;
