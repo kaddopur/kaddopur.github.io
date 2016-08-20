2216121708201fe119c456353e8bb1434a6764b1
Jason Huang <cjhuang@yahoo-inc.com>
2016-08-20 14:41:34 +0800

    add link to Logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router'
 import './Logo.css';
 
 const Logo = () => (
-  <div className="Logo">CHAOJU<br/>HUANG</div>
+  <Link to="/">
+    <div className="Logo">CHAOJU<br/>HUANG</div>
+  </Link>
 );
 
 export default Logo;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Router, Route, hashHistory } from 'react-router'
+
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <Router history={hashHistory}>
+    <Route path="/" component={App} />
+  </Router>,
   document.getElementById('root')
 );
