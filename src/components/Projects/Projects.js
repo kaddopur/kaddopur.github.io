f998bbf2f511e687dfcce7636a67d1ce30688932
Jason Huang <cjhuang@yahoo-inc.com>
2016-08-22 14:43:17 +0800

    add projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
 const Projects = () => (
-  <h3>Projects</h3>
+  <div>
+    <h3>Projects</h3>
+    <ul>
+      <li>Yahoo! TW/HK Homepage</li>
+      <li>Yhaoo! TW Video</li>
+      <li>Yahoo! TW News iOS</li>
+      <li>Yahoo! TW/HK Mobile Web</li>
+      <li>Yahoo! TV Scheduling Editorial Tool</li>
+      <li>Chrome Extension for daily developing</li>
+      <li>CLI for daily developing</li>
+    </ul>
+  </div>
 );
 
 export default Projects;
